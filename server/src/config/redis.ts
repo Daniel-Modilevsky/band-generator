7df60ff076cf13567580b5487d46ba4da3f5be20
“Daniel <daniel.modilevsky@gmail.com>
2025-02-12 09:40:40 +0200

    Update docker compose and redis connection

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -4,9 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const redis = new Redis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
+  host: process.env.REDIS_HOST || "redis_cache",
   port: Number(process.env.REDIS_PORT) || 6379,
 });
+
+redis.on("error", (err) => console.error("Redis Error:", err));
 
 setInterval(async () => {
   console.log("Cleaning expired API keys");
